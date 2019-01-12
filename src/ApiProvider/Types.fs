9c5f976ac586741789758ec837385146d41329d8
Petr Chromec <mortal_flesh@seznam.cz>
2019-01-12 20:16:10 +0100

    Parse checklist

---
+++ src/ApiProvider/Types.fs
@@ -0,0 +1,120 @@
+namespace ApiProvider
+
+type SingleCell = private SingleCell of string // A1
+type RangeCell = private RangeCell of string // A2:A5
+
+module SingleCell =
+    let value (SingleCell single) = single
+    let create (single: string) =
+        if single.Contains(':') then failwithf "Single cell must not contain :"
+        SingleCell single
+
+module RangeCell =
+    let value (RangeCell range) = range
+    let create (range: string) =
+        if range.Contains(':') |> not then failwithf "Range cells must contain :"
+        RangeCell range
+
+type Cell =
+    | Single of SingleCell
+    | Range of RangeCell
+
+// ===========================
+// Items
+// ===========================
+
+type Item = {
+    Label: string
+    Id: int
+}
+
+type SkippedItem = {
+    Label: string
+}
+
+type ItemOrSkipped =
+    | Item of Item
+    | Skipped of SkippedItem
+
+[<RequireQualifiedAccessAttribute>]
+module Item =
+    let getId { Id = id} = id
+
+[<RequireQualifiedAccessAttribute>]
+module ItemOrSkipped =
+    let getId = function
+        | Item item -> item |> Item.getId |> Some
+        | Skipped _ -> None
+
+// ===========================
+// Count (from inventories, bank, trading post delivery, ...)
+// ===========================
+
+type CountableItem = {
+    Item: Item
+    Cell: SingleCell
+}
+
+type CountableItemList = {
+    Label: string
+    Items: ItemOrSkipped list
+    Cell: RangeCell
+}
+
+type ItemToCount =
+    | Single of CountableItem
+    | Many of CountableItemList
+
+// ===========================
+// Known (recipe)
+// ===========================
+
+type Recipe = {
+    Item: Item
+    Cell: SingleCell
+    Value: int
+}
+
+// ===========================
+// Price (from trading post)
+// ===========================
+
+type PriceableItem = {
+    Item: Item
+    Cell: SingleCell
+}
+
+type PriceableItemList = {
+    Label: string
+    Items: ItemOrSkipped list
+    Cell: RangeCell
+}
+
+type ItemToPrice =
+    | Single of PriceableItem
+    | Many of PriceableItemList
+
+[<RequireQualifiedAccessAttribute>]
+module PriceableItemList =
+    let getItems ({ Items = items }: PriceableItemList) = items
+
+// ===========================
+// Currency (from wallet)
+// ===========================
+
+type Currency = {
+    Label: string
+    Id: int
+    Cell: SingleCell
+}
+
+// ===========================
+// Checklist
+// ===========================
+
+type Checklist = {
+    Count: ItemToCount list
+    Known: Recipe list
+    Price: ItemToPrice list
+    Currency: Currency list
+}
--- src/App/Program.fs
+++ src/App/Program.fs
@@ -2,19 +2,58 @@
 
 open System
 open MF.ConsoleStyle
+open Environment
+open ApiProvider
+open ApiProvider.ChecklistParser
 open ApiProvider.Characters
+open ApiProvider
 
-[<EntryPoint>]
-let main argv =
-    Console.title "Hello from Guild Wars helper"
-
-    let tableRows data =
+let tableRows data =
         data
         |> List.map Seq.ofList
         |> Seq.ofList
 
-    fetchCharacters()
-    |> tableRows
-    |> Console.table ["Name"]
+let showLinksForTradingPost checklistPrice =
+    Console.section "Prices for checklist (link to trading post)"
+
+    let idsToPrice =
+        checklistPrice
+        |> Seq.collect (function
+            | Single item -> [item.Item.Id]
+            | Many items ->
+                items
+                |> PriceableItemList.getItems
+                |> List.choose ItemOrSkipped.getId
+        )
+        |> Seq.distinct
+        |> Seq.map string
+        |> List.ofSeq
+
+    idsToPrice
+    |> Seq.length
+    |> printfn "Total items to price: %i\n"
+
+    idsToPrice
+    |> List.chunkBySize 16
+    |> List.iter ((String.concat ",") >> (printfn "https://www.gw2tp.com/custom-list?name=LIST_NAME&ids=%s\n"))
+
+[<EntryPoint>]
+let main argv =
+    Console.title "Guild Wars crafting helper"
+    let envs = ".env" |> getEnvs Console.error
+    let getEnv = getEnv envs
+    let tryGetEnv = tryGetEnv envs
+
+    //fetchCharacters()
+    //|> tableRows
+    //|> Console.table ["Name"]
+
+    let checklist =
+        "CHECKLIST"
+        |> getEnv
+        |> parseChecklist
+
+    checklist.Price
+    |> showLinksForTradingPost
 
     0
