fdbe45903285d39855fdb47f7b4eecddd36a1f7c
Petr Chromec <mortal_flesh@seznam.cz>
2019-03-19 19:17:08 +0100

    Add more checklist

--- updateSheetByApi.sh
+++ updateSheetByApi.sh
@@ -6,8 +6,20 @@ set -e
 
 # --------------------
 
-export LIST_NAME="Shining blade (lengendary sword)"
-export CHECKLIST=".check-shining-blade.json"
+#export LIST_NAME="Shining blade (lengendary sword)"
+#export CHECKLIST=".check-shining-blade.json"
+
+#echo $LIST_NAME
+#echo "=========================="
+#echo
+
+#dotnet src/App/bin/Release/netcoreapp2.1/App.dll
+#php src/Sheets/Wrapper/app.php
+
+# --------------------
+
+export LIST_NAME="Frostfang (legendary axe)"
+export CHECKLIST=".check-frostfang.json"
 
 echo $LIST_NAME
 echo "=========================="
@@ -18,8 +30,8 @@ php src/Sheets/Wrapper/app.php
 
 # --------------------
 
-export LIST_NAME="Frostfang (legendary axe)"
-export CHECKLIST=".check-frostfang.json"
+export LIST_NAME="PVP armor (legendary armor)"
+export CHECKLIST=".check-pvp-armor.json"
 
 echo $LIST_NAME
 echo "=========================="
